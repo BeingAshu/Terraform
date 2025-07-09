eea172d8d0cf112bdc163933d14fdd100528db09
Ashutosh Jatale <79712095+BeingAshu@users.noreply.github.com>
2025-07-09 19:45:27 +0530

    Update s3.tf

--- Project1/s3.tf
+++ Project1/s3.tf
@@ -1,8 +1,8 @@
-resource "aws_s3_bucket" "examplebucket" {
-  bucket = "my-dev-bucket-3232"
+#resource "aws_s3_bucket" "examplebucket" {
+#  bucket = "my-dev-bucket-3232"
 
-  tags = {
-    Name = "devbucket"
-    Enviroment ="Dev"
-  }
-}
+ # tags = {
+  #  Name = "devbucket"
+   # Enviroment ="Dev"
+  #}
+#}
