a3744dbf1bc68c9d9bd171497a6a7658b3d49bff
Ashutosh Jatale <79712095+BeingAshu@users.noreply.github.com>
2025-07-09 20:05:30 +0530

    Delete Project1/Backend directory

--- Project1/Backend/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "backendbucket" {
-  default = "my-demo-backend-bucket222"
-  description = "this is remote backend bucket for tf state"
-}
-
-variable "backendtablename" {
-    default = "table-1"
-  
-}
